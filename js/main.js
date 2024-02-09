2c9a7859c6c351cef4df755dd9ab071effdb00c7
larisa <larica290501@mail.ru>
2024-02-09 16:28:54 +0700

    РЕДАКТИРОВАНИЕ КАРТОЧКИ

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ new Vue({
                 createdAt: new Date().toISOString().slice(0,10),
                 lastEdited: null
             },
+            editedTask: null,
+            editedTaskIndex: null,
             plannedTasks: [],
             inProgressTasks: [],
         }
@@ -16,17 +18,28 @@ new Vue({
     methods: {
         addTask() {
             this.plannedTasks.push({...this.newTask});
-            this.newTask = { title: '', description: '', deadline: '', createdAt: new Date().toISOString().slice(0,10), lastEdited: null };
+            this.newTask = {
+                title: '',
+                description: '',
+                deadline: '',
+                createdAt: new Date().toISOString().slice(0, 10),
+                lastEdited: null
+            };
         },
         deleteTask(taskIndex) {
             this.plannedTasks.splice(taskIndex, 1);
         },
-        editTask(taskIndex, updatedTask) {
-            this.plannedTasks[taskIndex] = {...updatedTask, lastEdited: new Date().toISOString().slice(0,10)};
+        startEditing(taskIndex) {
+            this.editedTask = {...this.plannedTasks[taskIndex]};
+            this.editedTaskIndex = taskIndex;
+        },
+        finishEditing(taskIndex) {
+            this.plannedTasks[taskIndex] = {...this.editedTask, lastEdited: new Date().toISOString().slice(0, 10)};
+            this.editedTask = null;
+            this.editedTaskIndex = null
         },
         moveToInProgress(taskIndex) {
             const taskToMove = this.plannedTasks.splice(taskIndex, 1)[0];
-            this.inProgressTasks.push(taskToMove);
         }
     }
 })
