66cfa409a44a40036e33b62e4e8daabe4508ff09
larisa <larica290501@mail.ru>
2024-02-09 17:20:04 +0700

    ДОБАВЛЕНИЕ КАРТОЧКИ В ТЕСТИРОВАНИЕ И В ЗАВЕРШЕННОЕ

--- js/main.js
+++ js/main.js
@@ -15,34 +15,46 @@ new Vue({
             plannedTasks: [],
             inProgressTasks: [],
             testingTasks: [],
+            completedTasks: [],
         }
     },
     methods: {
         addTask() {
             this.plannedTasks.push({...this.newTask});
-            this.newTask = { title: '', description: '', deadline: '', createdAt: new Date().toLocaleString(), lastEdited: null };
+            this.newTask = {
+                title: '',
+                description: '',
+                deadline: '',
+                createdAt: new Date().toLocaleString(),
+                lastEdited: null
+            };
         },
         deleteTask(taskIndex) {
             this.plannedTasks.splice(taskIndex, 1);
         },
-            startEditing(taskIndex, column) {
-                this.editedTask = {...this[column][taskIndex]};
-                this.editedTaskIndex = taskIndex;
-                this.editedColumn = column;
-            },
-            finishEditing(taskIndex) {
-                this[this.editedColumn][taskIndex] = {...this.editedTask, lastEdited: new Date().toLocaleString()};
-                this.editedTask = null;
-                this.editedTaskIndex = null;
-                this.editedColumn = null;
-            },
-            moveToInProgress(taskIndex) {
-                const taskToMove = this.plannedTasks.splice(taskIndex, 1)[0];
-                this.inProgressTasks.push(taskToMove);
-            },
-            moveToTesting(taskIndex) {
-                const taskToMove = this.inProgressTasks.splice(taskIndex, 1)[0];
-                this.testingTasks.push(taskToMove);
-            }
+        startEditing(taskIndex, column) {
+            this.editedTask = {...this[column][taskIndex]};
+            this.editedTaskIndex = taskIndex;
+            this.editedColumn = column;
+        },
+        finishEditing(taskIndex) {
+            this[this.editedColumn][taskIndex] = {...this.editedTask, lastEdited: new Date().toLocaleString()};
+            this.editedTask = null;
+            this.editedTaskIndex = null;
+            this.editedColumn = null;
+        },
+        moveToInProgress(taskIndex) {
+            const taskToMove = this.plannedTasks.splice(taskIndex, 1)[0];
+            this.inProgressTasks.push(taskToMove);
+        },
+        moveToTesting(taskIndex) {
+            const taskToMove = this.inProgressTasks.splice(taskIndex, 1)[0];
+            this.testingTasks.push(taskToMove);
+        },
+        moveToCompleted(taskIndex) {
+            const taskToMove = this.testingTasks.splice(taskIndex, 1)[0];
+            this.completedTasks.push(taskToMove);
         }
+    }
+
     })
