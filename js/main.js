9de674f1f9dd4e515f7b542cf4bd78a0b0c0a153
larisa <larica290501@mail.ru>
2024-02-09 16:51:50 +0700

    ДОБАВЛЕНИЕ КАРТОЧКИ В РАБОТУ

--- js/main.js
+++ js/main.js
@@ -6,40 +6,43 @@ new Vue({
                 title: '',
                 description: '',
                 deadline: '',
-                createdAt: new Date().toISOString().slice(0,10),
+                reatedAt: new Date().toLocaleString(),
                 lastEdited: null
             },
             editedTask: null,
             editedTaskIndex: null,
+            editedColumn: null,
             plannedTasks: [],
             inProgressTasks: [],
+            testingTasks: [],
         }
     },
     methods: {
         addTask() {
             this.plannedTasks.push({...this.newTask});
-            this.newTask = {
-                title: '',
-                description: '',
-                deadline: '',
-                createdAt: new Date().toISOString().slice(0, 10),
-                lastEdited: null
-            };
+            this.newTask = { title: '', description: '', deadline: '', createdAt: new Date().toLocaleString(), lastEdited: null };
         },
         deleteTask(taskIndex) {
             this.plannedTasks.splice(taskIndex, 1);
         },
-        startEditing(taskIndex) {
-            this.editedTask = {...this.plannedTasks[taskIndex]};
-            this.editedTaskIndex = taskIndex;
-        },
-        finishEditing(taskIndex) {
-            this.plannedTasks[taskIndex] = {...this.editedTask, lastEdited: new Date().toISOString().slice(0, 10)};
-            this.editedTask = null;
-            this.editedTaskIndex = null
-        },
-        moveToInProgress(taskIndex) {
-            const taskToMove = this.plannedTasks.splice(taskIndex, 1)[0];
+            startEditing(taskIndex, column) {
+                this.editedTask = {...this[column][taskIndex]};
+                this.editedTaskIndex = taskIndex;
+                this.editedColumn = column;
+            },
+            finishEditing(taskIndex) {
+                this[this.editedColumn][taskIndex] = {...this.editedTask, lastEdited: new Date().toLocaleString()};
+                this.editedTask = null;
+                this.editedTaskIndex = null;
+                this.editedColumn = null;
+            },
+            moveToInProgress(taskIndex) {
+                const taskToMove = this.plannedTasks.splice(taskIndex, 1)[0];
+                this.inProgressTasks.push(taskToMove);
+            },
+            moveToTesting(taskIndex) {
+                const taskToMove = this.inProgressTasks.splice(taskIndex, 1)[0];
+                this.testingTasks.push(taskToMove);
+            }
         }
-    }
-})
+    })
