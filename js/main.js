19ff053c26c9390fd8b403745ce999a234fd4825
larisa <larica290501@mail.ru>
2024-02-09 16:19:53 +0700

    ДОБАВЛНИЕ КАРТОЧЕК

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
-new vue({
+new Vue({
     el: '#app',
     data(){
-
+        return {
+            newTask: {
+                title: '',
+                description: '',
+                deadline: '',
+                createdAt: new Date().toISOString().slice(0,10),
+                lastEdited: null
+            },
+            plannedTasks: [],
+            inProgressTasks: [],
+        }
     },
-    methods:{
-
+    methods: {
+        addTask() {
+            this.plannedTasks.push({...this.newTask});
+            this.newTask = { title: '', description: '', deadline: '', createdAt: new Date().toISOString().slice(0,10), lastEdited: null };
+        },
+        deleteTask(taskIndex) {
+            this.plannedTasks.splice(taskIndex, 1);
+        },
+        editTask(taskIndex, updatedTask) {
+            this.plannedTasks[taskIndex] = {...updatedTask, lastEdited: new Date().toISOString().slice(0,10)};
+        },
+        moveToInProgress(taskIndex) {
+            const taskToMove = this.plannedTasks.splice(taskIndex, 1)[0];
+            this.inProgressTasks.push(taskToMove);
+        }
     }
 })
